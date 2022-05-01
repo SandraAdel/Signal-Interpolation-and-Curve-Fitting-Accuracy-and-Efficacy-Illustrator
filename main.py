977078e947d05ec0985ca8ad645303af7510bee0
FarahGamal <farah.ali02@eng-st.cu.edu.eg>
2022-05-01 17:00:05 +0200

    edits in GUI+main

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from ast import increment_lineno
+from math import ceil
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
@@ -6,6 +8,7 @@ from pyqtgraph import PlotWidget
 import pandas as pd
 import numpy as np
 from sympy import rad
+import more_itertools as mit
 from GUI import Ui_MainWindow
 
 
@@ -21,9 +24,11 @@ class MainWindow(QMainWindow):
         self.AmplitudeReadings = []
         # self.data_frame = [0,0]
         self.signalPoint = 1000
-        self.oneChunck = 0
+        self.chunkSize = 0
+        self.oneChunk = 0
         self.result = []
 
+        self.ui.polynomialNumberOfChunksSpinBox.setValue(1)
         # Hide error map elements
         self.ui.errorMapGraphicsView.hide()
         self.ui.errorLabel.hide()
@@ -32,18 +37,18 @@ class MainWindow(QMainWindow):
         self.ui.polynomialFitPushButton.hide()
         self.ui.polynomialFittingOrderLabel.hide()
         self.ui.polynomialFittingOrderSpinBox.hide()
-        self.ui.polynomialNumberOfChuncksLabel.hide()
-        self.ui.polynomialNumberOfChuncksSpinBox.hide()
+        self.ui.polynomialNumberOfChunksLabel.hide()
+        self.ui.polynomialNumberOfChunksSpinBox.hide()
         self.ui.polynomialOerlapSpinBox.hide()
         self.ui.polynomialOverlapLabel.hide()
-        self.ui.polynomialOneChunckRadioButton.hide()
-        self.ui.polynomialMultipleChuncksRadioButton.hide()
+        self.ui.polynomialOneChunkRadioButton.hide()
+        self.ui.polynomialMultipleChunksRadioButton.hide()
         
         # Links of GUI Elements to Methods:
         self.ui.openAction.triggered.connect(lambda: self.OpenFile())
         self.ui.interpolationMethodComboBox.currentIndexChanged.connect(lambda: self.interpolatioMethodes(self.ui.interpolationMethodComboBox.currentIndex()))
-        self.ui.polynomialMultipleChuncksRadioButton.toggled.connect(lambda: self.radioButtonMultiple())
-        self.ui.polynomialOneChunckRadioButton.toggled.connect(lambda: self.radioButton())
+        self.ui.polynomialMultipleChunksRadioButton.toggled.connect(lambda: self.radioButtonMultiple())
+        self.ui.polynomialOneChunkRadioButton.toggled.connect(lambda: self.radioButton())
         self.ui.polynomialFitPushButton.clicked.connect(lambda: self.interpolation())
 
 
@@ -57,42 +62,53 @@ class MainWindow(QMainWindow):
     
     def interpolatioMethodes(self, index):
         if index == 0:
-            self.ui.polynomialOneChunckRadioButton.show()
-            self.ui.polynomialMultipleChuncksRadioButton.show()
+            self.ui.polynomialOneChunkRadioButton.show()
+            self.ui.polynomialMultipleChunksRadioButton.show()
             self.ui.polynomialFitPushButton.show()
             self.ui.polynomialFittingOrderLabel.show()
             self.ui.polynomialFittingOrderSpinBox.show()
             
     #! COODE REPETITION TACK CAAAAAAAAAAAAAAARE        
     def radioButtonMultiple (self):
-        if self.ui.polynomialMultipleChuncksRadioButton.isChecked():
-            self.ui.polynomialNumberOfChuncksLabel.show()
-            self.ui.polynomialNumberOfChuncksSpinBox.show()
+        if self.ui.polynomialMultipleChunksRadioButton.isChecked():
+            self.ui.polynomialNumberOfChunksLabel.show()
+            self.ui.polynomialNumberOfChunksSpinBox.show()
             self.ui.polynomialOerlapSpinBox.show()
             self.ui.polynomialOverlapLabel.show()
 
 
     def radioButton (self):
-        if self.ui.polynomialOneChunckRadioButton.isChecked():
+        if self.ui.polynomialOneChunkRadioButton.isChecked():
             self.oneChunck = 1
-            self.ui.polynomialNumberOfChuncksLabel.hide()
-            self.ui.polynomialNumberOfChuncksSpinBox.hide()
+            self.ui.polynomialNumberOfChunksLabel.hide()
+            self.ui.polynomialNumberOfChunksSpinBox.hide()
             self.ui.polynomialOerlapSpinBox.hide()
             self.ui.polynomialOverlapLabel.hide()
 
 
 
     def interpolation (self):
-            self.order = self.ui.polynomialFittingOrderSpinBox.value()
-            self.coeff = np.polyfit(self.TimeReadings, self.AmplitudeReadings,self.order)
+        self.chunckSize = ceil(1000/self.ui.polynomialNumberOfChunksSpinBox.value())
+        print(self.chunckSize)
+        self.order = self.ui.polynomialFittingOrderSpinBox.value()
+        self.ui.mainGraphGraphicsView.clear()
+        # overall point 1001 one empty 
+        for i in range(0,len(self.TimeReadings)-1,self.chunckSize):
+            amplitude = []
+            time = []
+            increment = i
+            for j in range(self.chunckSize-1):
+                if increment < len(self.TimeReadings):
+                    amplitude.append(self.AmplitudeReadings[increment])
+                    time.append(self.TimeReadings[increment])
+                    increment += 1
+            self.coeff = np.polyfit(time[0:int(self.chunckSize-1)], amplitude[0:int(self.chunckSize-1)],self.order)
             self.poly1d_fn = np.poly1d(self.coeff) 
-            self.ui.mainGraphGraphicsView.clear() 
+            #! COODE REPETITION TACK CAAAAAAAAAAAAAAARE
             self.ui.mainGraphGraphicsView.plot(self.TimeReadings, self.AmplitudeReadings, pen=pyqtgraph.mkPen('b', width=1.5))
-            self.ui.mainGraphGraphicsView.plot(self.TimeReadings, self.poly1d_fn(self.TimeReadings), pen=pyqtgraph.mkPen('g', width=1.5, style = QtCore.Qt.DotLine))
+            self.ui.mainGraphGraphicsView.plot(time, self.poly1d_fn(time), pen=pyqtgraph.mkPen('g', width=1.5, style = QtCore.Qt.DotLine))
 
     
-
-
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     win = MainWindow()
