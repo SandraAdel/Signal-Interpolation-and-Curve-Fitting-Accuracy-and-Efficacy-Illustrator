e269a36efa21970a551b1104c0a5f5a1312b0877
FarahGamal <farah.ali02@eng-st.cu.edu.eg>
2022-05-04 20:25:55 +0200

    edit

--- GUI.py
+++ GUI.py
@@ -10,6 +10,31 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
+class Slider(QtWidgets.QSlider):
+    def mousePressEvent(self, event):
+        super(Slider, self).mousePressEvent(event)
+        if event.button() == QtCore.Qt.LeftButton:
+            val = self.pixelPosToRangeValue(event.pos())
+            self.setValue(val)
+
+    def pixelPosToRangeValue(self, pos):
+        opt = QtWidgets.QStyleOptionSlider()
+        self.initStyleOption(opt)
+        gr = self.style().subControlRect(QtWidgets.QStyle.CC_Slider, opt, QtWidgets.QStyle.SC_SliderGroove, self)
+        sr = self.style().subControlRect(QtWidgets.QStyle.CC_Slider, opt, QtWidgets.QStyle.SC_SliderHandle, self)
+
+        if self.orientation() == QtCore.Qt.Horizontal:
+            sliderLength = sr.width()
+            sliderMin = gr.x()
+            sliderMax = gr.right() - sliderLength + 1
+        else:
+            sliderLength = sr.height()
+            sliderMin = gr.y()
+            sliderMax = gr.bottom() - sliderLength + 1;
+        pr = pos - sr.center() + sr.topLeft()
+        p = pr.x() if self.orientation() == QtCore.Qt.Horizontal else pr.y()
+        return QtWidgets.QStyle.sliderValueFromPosition(self.minimum(), self.maximum(), p - sliderMin,
+                                               sliderMax - sliderMin, opt.upsideDown)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -112,7 +137,7 @@ class Ui_MainWindow(object):
         self.extrapolationLabel.setAlignment(QtCore.Qt.AlignCenter)
         self.extrapolationLabel.setObjectName("extrapolationLabel")
         self.gridLayout_2.addWidget(self.extrapolationLabel, 0, 0, 1, 1)
-        self.extrapolationHorizontalSlider = QtWidgets.QSlider(self.widget)
+        self.extrapolationHorizontalSlider = Slider(self.widget)
         self.extrapolationHorizontalSlider.setMaximum(100)
         self.extrapolationHorizontalSlider.setOrientation(QtCore.Qt.Horizontal)
         self.extrapolationHorizontalSlider.setObjectName("extrapolationHorizontalSlider")
